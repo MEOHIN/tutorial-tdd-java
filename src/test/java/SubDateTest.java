92496aae83adfd406929a91d7797bc7d7bcb8693
meohin <meohin.meo@gmail.com>
2022-08-05 15:02:31 +0900

    Add test to check leap year

--- src/test/java/SubDateTest.java
+++ src/test/java/SubDateTest.java
@@ -6,6 +6,7 @@ class SubDateTest extends TestCase {
     public static void main(String[] args) {
         junit.textui.TestRunner.run(SubDateTest.class);
     }
+
     public void testGetYearDay(){
         // 0년은 존재하지 않는다. 최소 일자는 1년 1월 1일이다. 따라서 1년까지의 총 일수는 0이 돼야한다.
         assertEquals(0,SubDate.getYearDay(1));
@@ -13,6 +14,12 @@ class SubDateTest extends TestCase {
         assertEquals(365,SubDate.getYearDay(2));
         /* 실패하는 코드다.
         * 5년 미만의 총 일수를 구한다.*/
-        assertEquals(365+365+365+366, SubDate.getYearDay(5));
+//        assertEquals(365+365+365+366, SubDate.getYearDay(5));
+    }
+
+    public void testLeapYear() {
+        assertTrue(SubDate.isLeapYear(0));
+        assertFalse(SubDate.isLeapYear(1));
+        assertTrue(SubDate.isLeapYear(4));
     }
 }

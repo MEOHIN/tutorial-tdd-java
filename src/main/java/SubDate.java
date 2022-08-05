85195f59cdc8504ae23e09e902381c07cfd453fd
meohin <meohin.meo@gmail.com>
2022-08-05 15:36:35 +0900

    Update leap year method to pass test

--- src/main/java/SubDate.java
+++ src/main/java/SubDate.java
@@ -5,6 +5,9 @@ public class SubDate {
     }
 
     public static boolean isLeapYear(int year) {
-        return year % 4 == 0;
+        if (year % 400 == 0) return true;
+        if (year % 100 == 0) return false;
+        if (year % 4 == 0) return true;
+        return false;
     }
 }

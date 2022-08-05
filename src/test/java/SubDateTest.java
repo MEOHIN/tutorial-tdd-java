4e286280072ab229995db13dddb52f2e8aa71d21
meohin <meohin.meo@gmail.com>
2022-08-05 14:52:36 +0900

    Add documentation for method

--- src/test/java/SubDateTest.java
+++ src/test/java/SubDateTest.java
@@ -7,7 +7,9 @@ class SubDateTest extends TestCase {
         junit.textui.TestRunner.run(SubDateTest.class);
     }
     public void testGetYearDay(){
+        // 0년은 존재하지 않는다. 최소 일자는 1년 1월 1일이다. 따라서 1년까지의 총 일수는 0이 돼야한다.
         assertEquals(0,SubDate.getYearDay(1));
+        // 2년까지의 총 일수는 1년 1월 1일부터 2년 1월 1일까지다. 따라서 365일이다.
         assertEquals(365,SubDate.getYearDay(2));
     }
 }
